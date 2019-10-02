61add5aa89afa3eb474937bc564ac3b86ae5364a
Emmanuel Rosario <erosario1096@gmail.com>
2019-10-02 19:50:44 -0400

    another fix 2

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-const serverless = require("serverless-http");
-
 const app = express();
 
 //Init Middleware
@@ -10,7 +8,6 @@ app.use(express.json({ extended: true }));
 //Define Routes
 app.use("/api/breweries", require("./routes/breweries"));
 app.use("/api/google", require("./routes/google"));
-app.use("./netlify/functions/api", router);
 
 //Init Port number
 const PORT = process.env.PORT || 5000;
@@ -18,5 +15,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on Port: ${PORT}`);
 });
-
-module.exports.handler = serverless(app);
